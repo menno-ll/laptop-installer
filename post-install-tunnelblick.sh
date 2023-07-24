8084367a9de57445c237db8071434554d285df20
menno-ll <menno@level-level.com>
2023-07-24 15:17:01 +0200

    add tunnelblick post-install

---
+++ post-install-tunnelblick.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+generateNotificationBanner "Running post-install script of $1..."
+
+echo "Please upload your VPN profiles. 
+read -p "When you have successfully uploaded your profiles, press the return key: " -r TUNNELBLICK_PROFILES_CONFIGURED
+echo
+
