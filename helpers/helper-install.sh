e5fc83ccaede0b61de17ba9b67feff587562f14f
menno-ll <menno@level-level.com>
2023-07-24 15:49:13 +0200

    move into folders

--- helpers/helper-install.sh
+++ helpers/helper-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 function runPostInstall {
-	local POST_INSTALL_SCRIPT="post-install-$1.sh"
+	local POST_INSTALL_SCRIPT="$SCRIPT_DIRECTORY/post-installers/post-install-$1.sh"
 	if [ -f "${POST_INSTALL_SCRIPT}" ]; then
 		echo "Running post install script for $1..."
 		source "${POST_INSTALL_SCRIPT}" "$1"
@@ -9,7 +9,7 @@ function runPostInstall {
 }
 
 function runPostUpdate {
-	local POST_UPDATE_SCRIPT="post-update-$1.sh"
+	local POST_UPDATE_SCRIPT="$SCRIPT_DIRECTORY/post-updaters/post-update-$1.sh"
 	if [ -f "${POST_UPDATE_SCRIPT}" ]; then
 		echo "Running post update script for $1..."
 		source "${POST_UPDATE_SCRIPT}" "$1"
--- init.sh
+++ init.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -x
 
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo $SCRIPT_DIRECTORY;
 
 # Load functions
-source "helper-message.sh"
-source "helper-install.sh"
+source "$SCRIPT_DIRECTORY/helpers/helper-message.sh"
+source "$SCRIPT_DIRECTORY/helpers/helper-install.sh"
 
 generateWelcomeMessage
 
@@ -18,73 +17,73 @@ if [[ ! $TERMINAL_HAS_APP_MANAGEMENT_PERMISSIONS =~ ^[Yy]$ ]]; then
 fi
 
 # Setup homebrew
-source "install-brew.sh" "brew"
+source "$SCRIPT_DIRECTORY/installers/install-brew.sh" "brew"
 
 # Setup terminal
-source "install-xcode-select.sh" "xcode-select"
-source "install-zsh.sh" "zsh"
-source "install-oh-my-zsh.sh" "oh-my-zsh"
-source "install-nvm.sh" "nvm"
+source "$SCRIPT_DIRECTORY/installers/install-xcode-select.sh" "xcode-select"
+source "$SCRIPT_DIRECTORY/installers/install-zsh.sh" "zsh"
+source "$SCRIPT_DIRECTORY/installers/install-oh-my-zsh.sh" "oh-my-zsh"
+source "$SCRIPT_DIRECTORY/installers/install-nvm.sh" "nvm"
 
 # Install CLI tools
-source "install-brew-formula.sh" "wget"
-source "install-brew-formula.sh" "php"
-source "install-brew-formula.sh" "perl"
-source "install-brew-formula.sh" "composer"
-source "install-brew-formula.sh" "mysql@5.7"
-source "install-brew-formula.sh" "qcachegrind"
-source "install-brew-formula.sh" "jq"
-source "install-brew-formula.sh" "yq"
-source "install-brew-formula.sh" "wp-cli"
-source "install-brew-formula.sh" "thefuck"
+source "$SCRIPT_DIRECTORY/installers/install-brew-formula.sh" "wget"
+source "$SCRIPT_DIRECTORY/installers/install-brew-formula.sh" "php"
+source "$SCRIPT_DIRECTORY/installers/install-brew-formula.sh" "perl"
+source "$SCRIPT_DIRECTORY/installers/install-brew-formula.sh" "composer"
+source "$SCRIPT_DIRECTORY/installers/install-brew-formula.sh" "mysql@5.7"
+source "$SCRIPT_DIRECTORY/installers/install-brew-formula.sh" "qcachegrind"
+source "$SCRIPT_DIRECTORY/installers/install-brew-formula.sh" "jq"
+source "$SCRIPT_DIRECTORY/installers/install-brew-formula.sh" "yq"
+source "$SCRIPT_DIRECTORY/installers/install-brew-formula.sh" "wp-cli"
+source "$SCRIPT_DIRECTORY/installers/install-brew-formula.sh" "thefuck"
 
 # Install applications
-source "install-brew-cask.sh" "1password"
-source "install-brew-cask.sh" "adobe-acrobat-reader"
-source "install-brew-cask.sh" "android-studio"
-source "install-brew-cask.sh" "bitwarden"
-source "install-brew-cask.sh" "browserstacklocal"
-source "install-brew-cask.sh" "dash"
-source "install-brew-cask.sh" "discord"
-source "install-brew-cask.sh" "docker"
-source "install-brew-cask.sh" "firefox"
-source "install-brew-cask.sh" "google-chrome"
-source "install-brew-cask.sh" "handbrake"
-source "install-brew-cask.sh" "obs"
-source "install-brew-cask.sh" "poedit"
-source "install-brew-cask.sh" "postman"
-source "install-brew-cask.sh" "rectangle"
-source "install-brew-cask.sh" "sequel-ace"
-source "install-brew-cask.sh" "slack"
-source "install-brew-cask.sh" "soapui"
-source "install-brew-cask.sh" "spotify"
-source "install-brew-cask.sh" "teamviewer"
-source "install-brew-cask.sh" "the-unarchiver"
-source "install-brew-cask.sh" "tunnelblick"
-source "install-brew-cask.sh" "vagrant"
-source "install-brew-cask.sh" "virtualbox"
-source "install-brew-cask.sh" "visual-studio-code"
-source "install-brew-cask.sh" "vlc"
-source "install-brew-cask.sh" "yubico-authenticator"
-source "install-brew-cask.sh" "zoom"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "1password"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "adobe-acrobat-reader"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "android-studio"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "bitwarden"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "browserstacklocal"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "dash"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "discord"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "docker"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "firefox"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "google-chrome"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "handbrake"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "obs"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "poedit"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "postman"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "rectangle"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "sequel-ace"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "slack"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "soapui"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "spotify"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "teamviewer"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "the-unarchiver"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "tunnelblick"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "vagrant"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "virtualbox"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "visual-studio-code"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "vlc"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "yubico-authenticator"
+source "$SCRIPT_DIRECTORY/installers/install-brew-cask.sh" "zoom"
 
 # Install App Sore Applications
-source "install-app-store-application.sh" "harvest" "https://apps.apple.com/us/app/harvest/id506189836"
-source "install-app-store-application.sh" "keynote" "https://apps.apple.com/us/app/keynote/id409183694"
-source "install-app-store-application.sh" "lightshot" "https://apps.apple.com/us/app/lightshot-screenshot/id526298438"
-source "install-app-store-application.sh" "numbers" "https://apps.apple.com/us/app/numbers/id409203825"
-source "install-app-store-application.sh" "pages" "https://apps.apple.com/us/app/pages/id409201541"
-source "install-app-store-application.sh" "xcode" "https://apps.apple.com/us/app/xcode/id497799835"
+source "$SCRIPT_DIRECTORY/installers/install-app-store-application.sh" "harvest" "https://apps.apple.com/us/app/harvest/id506189836"
+source "$SCRIPT_DIRECTORY/installers/install-app-store-application.sh" "keynote" "https://apps.apple.com/us/app/keynote/id409183694"
+source "$SCRIPT_DIRECTORY/installers/install-app-store-application.sh" "lightshot" "https://apps.apple.com/us/app/lightshot-screenshot/id526298438"
+source "$SCRIPT_DIRECTORY/installers/install-app-store-application.sh" "numbers" "https://apps.apple.com/us/app/numbers/id409203825"
+source "$SCRIPT_DIRECTORY/installers/install-app-store-application.sh" "pages" "https://apps.apple.com/us/app/pages/id409201541"
+source "$SCRIPT_DIRECTORY/installers/install-app-store-application.sh" "xcode" "https://apps.apple.com/us/app/xcode/id497799835"
 
 # Install manual applications
-source "install-manual-application.sh" "android-file-transfer" "https://www.android.com/filetransfer/"
+source "$SCRIPT_DIRECTORY/installers/install-manual-application.sh" "android-file-transfer" "https://www.android.com/filetransfer/"
 
 # Install browser shortcut applications
-source "install-browser-shortcut-application.sh" "google-calendar" "https://calendar.google.com/"
-source "install-browser-shortcut-application.sh" "google-keep" "https://keep.google.com/"
-source "install-browser-shortcut-application.sh" "voys" "https://webphone.voys.nl/"
+source "$SCRIPT_DIRECTORY/installers/install-browser-shortcut-application.sh" "google-calendar" "https://calendar.google.com/"
+source "$SCRIPT_DIRECTORY/installers/install-browser-shortcut-application.sh" "google-keep" "https://keep.google.com/"
+source "$SCRIPT_DIRECTORY/installers/install-browser-shortcut-application.sh" "voys" "https://webphone.voys.nl/"
 
 # Install Laravel Valet
-source "install-laravel-valet.sh" "laravel-valet"
+source "$SCRIPT_DIRECTORY/installers/install-laravel-valet.sh" "laravel-valet"
 
 generateGoodbyeMessage
