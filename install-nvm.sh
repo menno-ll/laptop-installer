75f573597d182fdeb3eba43366e5926167bd2c27
menno-ll <menno@level-level.com>
2023-07-24 15:28:52 +0200

    add nvm post install

--- install-nvm.sh
+++ install-nvm.sh
@@ -17,10 +17,6 @@ if [ "$(type -t nvm)" != "function" ]; then
         echo "Installing $1..."
         curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
-        export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
         runPostInstall "$1"
     fi
 else
---
+++ post-install-nvm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+generateNotificationBanner "Running post-install script of $1..."
+
+echo "Adding $1 to PATH..."
+
+echo "Should already be added to path, just make it available in current script instead."
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
