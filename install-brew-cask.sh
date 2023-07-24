67db3530e004f41448b38ceba3f1e820a36f119c
menno-ll <menno@level-level.com>
2023-07-24 14:36:49 +0200

    fix brew installed check

--- install-brew-cask.sh
+++ install-brew-cask.sh
@@ -2,7 +2,7 @@
 
 generateNotificationBanner "Checking your installation of $1..."
 
-if ! brew info $1 &>/dev/null; then
+if ! brew list --cask $1 &>/dev/null; then
     read -p "$1 is not installed. Do you want to install it? (y/n): " -n 1 -r INSTALL_BREW_CASK
     echo
 
@@ -19,7 +19,7 @@ else
     if [[ $UPDATE_BREW_CASK =~ ^[Yy]$ ]]; then
         echo "Updating $1..."
         brew upgrade --cask $1
-        
+
         runPostUpdate "$1"
     fi
 fi
--- install-brew-formula.sh
+++ install-brew-formula.sh
@@ -2,7 +2,7 @@
 
 generateNotificationBanner "Checking your installation of $1..."
 
-if ! brew info $1 &>/dev/null; then
+if ! brew list $1 &>/dev/null; then
     read -p "$1 is not installed. Do you want to install it? (y/n): " -n 1 -r INSTALL_BREW_FORMULA
     echo
 
