af24f157f8a4b1c44c749c2469758b9741f6ae57
menno-ll <menno@level-level.com>
2023-07-24 15:18:03 +0200

    fix echo

--- post-install-tunnelblick.sh
+++ post-install-tunnelblick.sh
@@ -2,7 +2,7 @@
 
 generateNotificationBanner "Running post-install script of $1..."
 
-echo "Please upload your VPN profiles. 
+echo "Please upload your VPN profiles."
 read -p "When you have successfully uploaded your profiles, press the return key: " -r TUNNELBLICK_PROFILES_CONFIGURED
 echo
 
--- post-install-visual-studio-code.sh
+++ post-install-visual-studio-code.sh
@@ -2,7 +2,7 @@
 
 generateNotificationBanner "Running post-install script of $1..."
 
-echo "Enable settings sync in VS Code by going to Code -> Settings... -> Settings Sync. 
+echo "Enable settings sync in VS Code by going to Code -> Settings... -> Settings Sync."
 read -p "When you have successfully enabled settings sync, press the return key: " -r VISUAL_STUDIO_CODE_SETTINGS_SYNC_CONFIGURED
 echo
 
