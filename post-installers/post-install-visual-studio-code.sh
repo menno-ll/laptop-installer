4a4a260209cc7f9202896b76d44d9ee9d81088f2
menno-ll <menno@level-level.com>
2023-07-24 17:14:58 +0200

    add shell command check

--- post-installers/post-install-visual-studio-code.sh
+++ post-installers/post-install-visual-studio-code.sh
@@ -6,3 +6,6 @@ echo "Enable settings sync in VS Code by going to Code -> Settings... -> Setting
 read -p "When you have successfully enabled settings sync, press the return key: " -r VISUAL_STUDIO_CODE_SETTINGS_SYNC_CONFIGURED
 echo
 
+echo "Enable the code command. Open the Command Palette (Cmd+Shift+P) and type 'shell command' to find the Shell Command: Install 'code' command in PATH command."
+read -p "When you have successfully installed the code shell command, press the return key: " -r VISUAL_STUDIO_CODE_SETTINGS_SYNC_CONFIGURED
+echo
