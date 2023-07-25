26b375c1e7589e0e131b68e173634e901a069ab4
menno-ll <menno@level-level.com>
2023-07-25 21:38:56 +0200

    laravel install in new terminal instance

--- installers/install-laravel-valet.sh
+++ installers/install-laravel-valet.sh
@@ -9,9 +9,24 @@ if [ ! -d "$HOME/.config/valet" ]; then
     if [[ $INSTALL_LARAVEL_VALET =~ ^[Yy]$ ]]; then
         echo "Installing $1..."
         composer global require laravel/valet
-        valet install
 
-        runPostInstall "$1"
+        if [ ! -x "$(command -v valet)" ]; then
+            echo 'Valet command is not available, adding Composer bin to PATH in an attempt to fix it.';
+            export PATH="$PATH:$HOME/.composer/vendor/bin"
+        fi
+
+        if [ ! -x "$(command -v valet)" ]; then
+            VALET_POST_INSTALL_SCRIPT="$SCRIPT_DIRECTORY/post-installers/post-install-$1.sh"
+            osascript -e "
+            tell application \"Terminal\"
+                set currentTab to do script \"valet install\"
+                do script \"sh $VALET_POST_INSTALL_SCRIPT $1\" in currentTab
+                do script \"echo 'Valet post install script has been completed. You may close this terminal tab.'\" in currentTab
+            end tell"
+        else
+            valet install
+            runPostInstall "$1"
+        fi
     fi
 else
     read -p "$1 is already installed. Do you want to update it? (y/n): " -n 1 -r UPDATE_LARAVEL_VALET
